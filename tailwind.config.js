12072fe1195b40e0c0a4ce764ab1dd80ae51aa05
Clement <bosstclement@gmail.com>
2025-02-14 13:52:11 +0200

    Added hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Adjust paths based on your project
+    content: ['./build/*.html'],
     theme: {
       extend: {
         fontFamily: {
