3a46e8224649e2daed89d2b9bb886f0dca987b2c
bossclement <bosstclement@gmail.com>
2025-02-14 17:20:43 +0200

    fixed a few things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./build/*.html'],
-    theme: {
-      extend: {
-        fontFamily: {
-            rob: ['Roboto'],
-        },
-        colors: {
-            primary: "var(--primary-color)",
-            secondary: "var(--secondary-color)",
-        },
-      },
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto'],
+        eb: ['EB+Garamond']
+      }
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+}
+
